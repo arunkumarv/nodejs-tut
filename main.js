7f38dd8b49885540ee7a3c1160c231c5fc216dc0
arunkumarv <gangaeyam@gmail.com>
2019-11-25 19:09:17 +0530

    User created modules - main.js, utils.js

---
+++ main.js
@@ -0,0 +1,3 @@
+var utils = require ('./utils') // .js is not required
+
+console.log ( utils.sum ( 3, 2 ) );
---
+++ utils.js
@@ -0,0 +1,5 @@
+var sum = function (a, b){ return a + b; };
+
+var mul = function (a, b){ return a * b; };
+
+module.exports = { sum, mul }
